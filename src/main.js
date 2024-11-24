caf9dabc5bc18b187118d298ea3b2ddc8333fe29
ReizalPutra <mohreizal27@gmail.com>
2024-11-24 09:09:22 +0700

    first commit, project setup

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+
+dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+    res.send("halooooo");
+});
+app.listen(port, ()=>{
+    console.log(`App listening on port ${port}`);
+});
